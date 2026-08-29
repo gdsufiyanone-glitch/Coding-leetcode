6849a7251a39d336dfdeb8b32231155fc19451c7
Md Sufiyan <gdsufiyanone@gmail.com>
2026-08-29 14:06:24 +0530

    Time: 6 ms (11.70%) | Memory: 263.7 MB (9.79%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
+        while (k >= nums.size())
+            k = k - nums.size();
         vector <int> subArray(nums.end() - k, nums.end());    
         nums.erase(nums.end() - k, nums.end());
         nums.insert(nums.begin(), subArray.begin(), subArray.end()); 
