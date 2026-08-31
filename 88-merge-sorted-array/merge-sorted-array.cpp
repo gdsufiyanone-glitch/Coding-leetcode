a4b64653b40812a701fa2941be0e8ee72260afaa
Md Sufiyan <gdsufiyanone@gmail.com>
2026-08-31 23:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (79.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        nums1.erase(nums1.begin()+m,nums1.end());
-        nums1.insert(nums1.begin(),nums2.begin(),nums2.end());
+        // nums1.erase(nums1.begin()+m,nums1.end());
+        // nums1.insert(nums1.begin(),nums2.begin(),nums2.end());
+        for (int i = 0 ; i < n ; i++){
+            nums1[m+i] = nums2[i];
+        }
         sort(nums1.begin(),nums1.end());
     }
 };
