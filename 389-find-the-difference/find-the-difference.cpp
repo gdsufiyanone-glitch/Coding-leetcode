bdc9c5aeefceb3cf833f635de1cf28130f427e64
Md Sufiyan <gdsufiyanone@gmail.com>
2026-09-04 10:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (15.21%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<char> vec1;
+        vector<char> vec2;
+        for(int i = 0 ; i < s.length(); i++){
+            vec1.emplace_back(s[i]);
+        }
+        sort(vec1.begin(),vec1.end());
+        
+        for(int i = 0 ; i < t.length(); i++){
+            vec2.emplace_back(t[i]);
+        }
+        sort(vec2.begin(),vec2.end());
+        for(int i = 0 ; i < t.length(); i++){
+            if(i < s.length() && vec1[i] != vec2[i] ){
+                return vec2[i];
+            }
+        }
+    
+        return vec2[t.length() - 1];
+    }
+        
+};
