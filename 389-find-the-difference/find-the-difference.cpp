34ea0d4539644586e199a9c66b9eb72be4aebdb5
Md Sufiyan <gdsufiyanone@gmail.com>
2026-09-04 11:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (95.67%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,24 +1,38 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<char> vec1;
-        vector<char> vec2;
-        for(int i = 0 ; i < s.length(); i++){
-            vec1.emplace_back(s[i]);
-        }
-        sort(vec1.begin(),vec1.end());
+        // vector<char> vec1;
+        // vector<char> vec2;
+        // for(int i = 0 ; i < s.length(); i++){
+        //     vec1.emplace_back(s[i]);
+        // }
+        // sort(vec1.begin(),vec1.end());
         
-        for(int i = 0 ; i < t.length(); i++){
-            vec2.emplace_back(t[i]);
-        }
-        sort(vec2.begin(),vec2.end());
-        for(int i = 0 ; i < t.length(); i++){
-            if(i < s.length() && vec1[i] != vec2[i] ){
-                return vec2[i];
-            }
-        }
+        // for(int i = 0 ; i < t.length(); i++){
+        //     vec2.emplace_back(t[i]);
+        // }
+        // sort(vec2.begin(),vec2.end());
+        // for(int i = 0 ; i < t.length(); i++){
+        //     if(i < s.length() && vec1[i] != vec2[i] ){
+        //         return vec2[i];
+        //     }
+        // }
     
-        return vec2[t.length() - 1];
+        // return vec2[t.length() - 1];
+
+
+
+        // using Xor 
+        int a = 0;
+        for(int i = 0 ; i < t.length();i++){
+            a = a ^ s[i] ^ t[i];
+        }
+        if(a == 0){
+            return t[t.length() - 1];
+        }else{
+            return char(a);
+        }
     }
         
 };
